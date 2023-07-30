c5ac9ed082c022ee6541fc1e7182382caeec9cb2
makeshmakesh <mmakesh98762gmail.com>
2023-07-30 13:20:51 +0530

    data base

--- makechat/settings.py
+++ makechat/settings.py
@@ -77,28 +77,13 @@ WSGI_APPLICATION = 'makechat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'makechat',
-
-        'USER': 'makechat',
-
-        'PASSWORD': 'Mein9876',
-
-        'HOST': 'database-1.ccnaacovctj2.us-west-2.rds.amazonaws.com',
-
-        'PORT': '5432',
-        # ..
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
